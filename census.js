2db8cbc3f341eac3cc40976d83f40f5c41cddbd9
Bogdan <zarechnyy00@internet.ru>
2024-12-16 17:54:34 +0300

    Add comment in lucke-ticket, create census

---
+++ census.js
@@ -0,0 +1,23 @@
+function census(arr) {
+  if (arr.length <= 1) return undefined;
+  const pers = arr.reduce((acc, item) => {
+    console.log(acc, item);
+    if (acc.age > item.age) {
+      return acc;
+    } else {
+      return item;
+    }
+  });
+
+  return pers.name;
+}
+
+console.log(
+  census([
+    { age: 12, name: 'Bob', gender: 'Male' },
+    { age: 37, name: 'Liza', gender: 'Female' },
+    { age: 80, name: 'Liza', gender: 'Female' },
+    { age: 40, name: 'Foo', gender: 'Male' },
+  ])
+); // 'Foo'
+console.log(census([{ age: 40, name: 'Liza', gender: 'Female' }])); // 'undefined'
