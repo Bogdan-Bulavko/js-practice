133aa1c656a4c1c88aa30792f50cbabafa6e533c
Bogdan <zarechnyy00@internet.ru>
2024-12-16 19:33:11 +0300

    Refactoring census

--- census.js
+++ census.js
@@ -2,10 +2,10 @@ function census(arr) {
   if (arr.length <= 1) return undefined;
 
   const pers = arr.reduce((acc, item) => {
-    if (acc.age > item.age) {
-      return acc;
+    if (item.gender === 'Male') {
+      return item.age > acc.age ? item : acc;
     } else {
-      return item;
+      return acc;
     }
   });
 
@@ -16,7 +16,8 @@ console.log(
   census([
     { age: 12, name: 'Bob', gender: 'Male' },
     { age: 37, name: 'Liza', gender: 'Female' },
-    { age: 80, name: 'Liza', gender: 'Female' },
+    { age: 80, name: 'Gera', gender: 'Female' },
+    { age: 70, name: 'Goo', gender: 'Male' },
     { age: 40, name: 'Foo', gender: 'Male' },
   ])
 ); // 'Foo'
