890c2eed2834194a312db39d3038b5f8f89821de
Bogdan <zarechnyy00@internet.ru>
2024-12-20 12:06:08 +0300

    Refactor lucky-ticket

--- lucky-ticket.js
+++ lucky-ticket.js
@@ -1,22 +1,16 @@
 function checkTicket(str) {
-  const arr = str.split('').map((item) => +item);
-  let numTotal = arr.reduce((acc, item) => acc + item); // Общее число
-  let num2 = 0; // Число второй половинки
-  for (let i = arr.length - 1; i >= 0; i--) {
-    // Перебираю массив с конца
-    numTotal -= arr[i]; // От общего числа отнимаю крайнее число
-    num2 += arr[i]; // К числу второй половинки прибавляю крайнее число
-    console.log(numTotal, num2);
-    if (numTotal === num2) {
-      return true;
-    }
-  }
-  /*С каждой итерацией общее число уменьшаеться, а число второй половинки увеличивается. 
-  Либо они будут равны, либо нет и не имеет значения четная длина массива или нет*/
-  return false;
-}
+  if (str.length % 2 !== 0) return false;
+  const firstHalfTicket = str
+    .split('')
+    .slice(0, str.length / 2)
+    .reduce((acc, item) => acc + +item, 0);
+  const seccondHalfTicket = str
+    .split('')
+    .slice(str.length / 2)
+    .reduce((acc, item) => acc + +item, 0);
 
-console.log(checkTicket('001 809')); // false
-// console.log(checkTicket('133700')); // true
-// console.log(checkTicket('123032')); // false
-// console.log(checkTicket('0052102')); // true
+  return firstHalfTicket === seccondHalfTicket ? true : false;
+}
+console.log(checkTicket('005212')); // true
+console.log(checkTicket('133700')); // true
+console.log(checkTicket('123032')); // false
