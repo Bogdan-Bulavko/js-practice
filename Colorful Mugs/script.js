753807b672bbd3c42c512a300f2740a15f935d2c
Bogdan <zarechnyy00@internet.ru>
2024-12-23 15:20:02 +0300

    Refactor Colorful Mugs

--- Colorful Mugs/script.js
+++ Colorful Mugs/script.js
@@ -7,7 +7,8 @@ const quantityCircle = 8;
 const additionalInformationAboutCircle = false;
 const dataCircle = [];
 const dataHex = [];
-
+let clickTop = 0;
+let clickLeft = 0;
 function generateHex(data) {
   const colorSymbol = [
     '0',
@@ -113,9 +114,6 @@ function mouseDownMoveElem(e) {
   }
 }
 
-let clickTop = 0;
-let clickLeft = 0;
-
 function setPositonInStyle(elem, position) {
   if (
     position.top < playingField.offsetHeight - elem.offsetHeight + clickTop &&
@@ -124,16 +122,112 @@ function setPositonInStyle(elem, position) {
     position.top - clickTop >= 0
   ) {
     if (clickTop === 0 && clickLeft === 0) {
+      // Генерация кружков при загрузке страницы
       elem.style.top = `${position.top}px`;
       elem.style.left = `${position.left}px`;
+    } else if (
+      // Генерация кружка при двойном щелчке когда top и left меньше ширины кружка в левом верхнем углу
+      clickTop === position.top &&
+      clickTop < sizeCircle &&
+      clickLeft === position.left &&
+      clickLeft < sizeCircle
+    ) {
+      elem.style.top = `${position.top - clickTop}px`;
+      elem.style.left = `${position.left - clickLeft}px`;
+    } else if (
+      // Генерация кружка при двойном щелчке если left > ширины кружка, но top < высоты кружка (верхняя граница)
+      clickTop === position.top &&
+      clickTop < sizeCircle &&
+      clickTop < playingField.clientHeight &&
+      clickLeft === position.left &&
+      clickLeft > sizeCircle &&
+      clickLeft < playingField.clientWidth - sizeCircle
+    ) {
+      elem.style.top = `${position.top - clickTop}px`;
+      elem.style.left = `${position.left - sizeCircle / 2}px`;
+    } else if (
+      //Генерация кружка при двойном щелчке если left < ширины кружка, но top < высоты поля (левый нижний угол)
+      clickTop === position.top &&
+      clickTop < playingField.clientHeight &&
+      clickTop > playingField.clientHeight - sizeCircle &&
+      clickLeft === position.left &&
+      clickLeft < sizeCircle
+    ) {
+      elem.style.top = `${playingField.clientHeight - sizeCircle}px`;
+      elem.style.left = `${0}px`;
+    } else if (
+      //Генерация кружка при двойном щелчке если left < ширины кружка, но top < высоты поля отняв высоту кружка(левая граница)
+      clickTop === position.top &&
+      clickTop < playingField.clientHeight - sizeCircle &&
+      clickLeft === position.left &&
+      position.left < sizeCircle
+    ) {
+      elem.style.top = `${position.top - sizeCircle / 2}px`;
+      elem.style.left = `${position.left - clickLeft}px`;
+    } else if (
+      //Генерация кружка при двойном щелчке если left < ширины поля, но больше ширины поля отняв ширину кружка, но top < высоты поля отняв высоту кружка (правый нижний угол)
+      clickTop === position.top &&
+      clickTop < playingField.clientHeight &&
+      clickTop > playingField.clientHeight - sizeCircle &&
+      clickLeft === position.left &&
+      clickLeft < playingField.clientWidth &&
+      clickLeft > playingField.clientWidth - sizeCircle
+    ) {
+      elem.style.top = `${playingField.clientHeight - sizeCircle}px`;
+      elem.style.left = `${playingField.clientWidth - sizeCircle}px`;
+    } else if (
+      //Генерация кружка при двойном щелчке если left > ширины кружка, но top < высоты поля отняв высоту кружка (нижняя граница)
+      clickTop === position.top &&
+      clickTop < playingField.clientHeight &&
+      clickTop > playingField.clientHeight - sizeCircle &&
+      clickLeft === position.left &&
+      clickLeft > sizeCircle
+    ) {
+      elem.style.top = `${playingField.clientHeight - sizeCircle}px`;
+      elem.style.left = `${position.left - sizeCircle / 2}px`;
+    } else if (
+      //Генерация кружка при двойном щелчке если left < ширины поля, но больше ширины поля отняв ширину кружка, и top < высоты поля (правая граница)
+      clickTop === position.top &&
+      clickTop < playingField.clientHeight &&
+      clickTop > sizeCircle &&
+      clickLeft === position.left &&
+      clickLeft < playingField.clientWidth &&
+      clickLeft > playingField.clientWidth - sizeCircle
+    ) {
+      elem.style.top = `${position.top - sizeCircle / 2}px`;
+      elem.style.left = `${playingField.clientWidth - sizeCircle}px`;
+    } else if (
+      //Генерация кружка при двойном щелчке если left < ширины поля, но больше ширины поля отняв ширину кружка, и top < высоты поля и меньше высоты кружка(правый верхний угол)
+      clickTop === position.top &&
+      clickTop < playingField.clientHeight &&
+      clickTop < sizeCircle &&
+      clickLeft === position.left &&
+      clickLeft < playingField.clientWidth &&
+      clickLeft > playingField.clientWidth - sizeCircle
+    ) {
+      elem.style.top = `${0}px`;
+      elem.style.left = `${playingField.clientWidth - sizeCircle}px`;
     } else if (clickTop === position.top && clickLeft === position.left) {
+      // Генерация кружка в свободном положении
       elem.style.top = `${position.top - sizeCircle / 2}px`;
       elem.style.left = `${position.left - sizeCircle / 2}px`;
     } else {
-      console.log(elem, position);
       elem.style.top = `${position.top - clickTop}px`;
       elem.style.left = `${position.left - clickLeft}px`;
     }
+  }
+}
+
+function addEndDeleteCircle(e) {
+  if (e.target.getAttribute('id') === 'playing-field') {
+    clickTop = e.y - e.target.getBoundingClientRect().top;
+    clickLeft = e.x - e.target.getBoundingClientRect().left;
+    createCercle();
+  } else {
+    e.target.remove();
+    dataCircle.filter((item) => {
+      return item.id !== '#' + e.target.getAttribute('id');
+    });
   }
 }
 
@@ -141,7 +235,6 @@ function addEventHandlersForCircle() {
   playingField.addEventListener('mousedown', (e) => {
     clickTop = e.y - e.target.getBoundingClientRect().top;
     clickLeft = e.x - e.target.getBoundingClientRect().left;
-
     playingField.addEventListener('mousemove', mouseDownMoveElem);
   });
   playingField.addEventListener('mouseup', () => {
@@ -149,11 +242,7 @@ function addEventHandlersForCircle() {
   });
 }
 
-playingField.addEventListener('dblclick', (e) => {
-  clickTop = e.y - e.target.getBoundingClientRect().top;
-  clickLeft = e.x - e.target.getBoundingClientRect().left;
-  createCercle();
-});
+playingField.addEventListener('dblclick', addEndDeleteCircle);
 
 generateMugs();
 addEventHandlersForCircle();
