b492022750ee05fbc2303708916596e8bb460447
Bogdan <zarechnyy00@internet.ru>
2025-01-23 16:19:18 +0300

    004 - Create a rendering function for rendering a type of Pokemon

--- pokemon/script.js
+++ pokemon/script.js
@@ -3,18 +3,114 @@ const POKEMON_API = 'https://pokeapi.co/api/v2';
 const POKEMON_LIMIT = 12;
 let pokemonOffset = 0;
 const START_ID = '#0000';
+const pokemonTypeDatabase = [
+  { typeName: 'Bug', backgroundColor: '#729f3f', textColor: '#fff' },
+  {
+    typeName: 'Dragon',
+    backgroundColor: 'linear-gradient(180deg, #53a4cf 50%, #f16e57 50%)',
+    textColor: '#fff',
+  },
+  {
+    typeName: 'Fairy',
+    backgroundColor: '#fdb9e9',
+    textColor: '#212121',
+  },
+  {
+    typeName: 'Fire',
+    backgroundColor: '#fd7d24',
+    textColor: '#fff',
+  },
+  {
+    typeName: 'Ghost',
+    backgroundColor: '#7b62a3',
+    textColor: '#fff',
+  },
+  {
+    typeName: 'Ground',
+    backgroundColor: 'linear-gradient(180deg, #f7de3f 50%, #ab9842 50%)',
+    textColor: '#212121',
+  },
+  {
+    typeName: 'Normal',
+    backgroundColor: '#a4acaf',
+    textColor: '#212121',
+  },
+  {
+    typeName: 'Psychic',
+    backgroundColor: '#a4acaf',
+    textColor: '#fff',
+  },
+  {
+    typeName: 'Steel',
+    backgroundColor: '#9eb7b8',
+    textColor: '#212121',
+  },
+  {
+    typeName: 'Dark',
+    backgroundColor: '#707070',
+    textColor: '#fff',
+  },
+  {
+    typeName: 'Electric',
+    backgroundColor: '#eed535',
+    textColor: '#212121',
+  },
+  {
+    typeName: 'Fighting',
+    backgroundColor: '#d56723',
+    textColor: '#fff',
+  },
+  {
+    typeName: 'Flying',
+    backgroundColor: 'linear-gradient(180deg, #3dc7ef 50%, #bdb9b8 50%)',
+    textColor: '#212121',
+  },
+  {
+    typeName: 'Grass',
+    backgroundColor: '#9bcc50',
+    textColor: '#212121',
+  },
+  {
+    typeName: 'Ice',
+    backgroundColor: '#51c4e7',
+    textColor: '#212121',
+  },
+  {
+    typeName: 'Poison',
+    backgroundColor: '#b97fc9',
+    textColor: '#fff',
+  },
+  {
+    typeName: 'Rock',
+    backgroundColor: '#a38c21',
+    textColor: '#fff',
+  },
+  {
+    typeName: 'Water',
+    backgroundColor: '#4592c4',
+    textColor: '#fff',
+  },
+];
 
 const cardCatalog = document.querySelector('.card-catalog');
 const cardCatalogButton = document.querySelector('.card-catalog__button');
 
-function generatePokemonCard(pokemon) {
-  const { img, id, name, setOfForces } = pokemon;
+const renderThePokemonType = function (pokemon, typeWrapper) {
+  pokemon.setOfForces.forEach((power) => {
+    const typeStyle = pokemonTypeDatabase.find((PT) => PT.typeName === power);
 
-  let TypeOfForce = '';
-
-  setOfForces.forEach((force) => {
-    TypeOfForce += `<div class="pokemon-card__type-of-force type-of-force--${force}">${force}</div>`;
+    console.log(typeStyle);
+    const type = document.createElement('div');
+    type.classList.add('pokemon-card__type-of-force');
+    type.textContent = power;
+    type.style.background = typeStyle.backgroundColor;
+    type.style.color = typeStyle.textColor;
+    typeWrapper.append(type);
   });
+};
+
+const generatePokemonCard = function (pokemon) {
+  const { img, id, name } = pokemon;
 
   cardCatalog.insertAdjacentHTML(
     'beforeend',
@@ -29,28 +125,25 @@ function generatePokemonCard(pokemon) {
               <h2 class="pokemon-card__name">${
                 name.length < 15 ? name : name.slice(0, 15) + '...'
               }</h2>
-              <div class="pokemon-card__set-of-forces">
-                    ${TypeOfForce}
-              </div>
+              <div class="pokemon-card__set-of-forces" id=${id}></div>
             </div>
           </div>`
   );
-}
+  renderThePokemonType(pokemon, document.getElementById(id));
+};
 
-async function addAllPokemonCards() {
+const addAllPokemonCards = async function () {
   let dataPokemons = await getPokemons();
   pokemonOffset += POKEMON_LIMIT;
   dataPokemons.forEach((pokemon) => generatePokemonCard(pokemon));
-}
-
-addAllPokemonCards();
+};
 
 const fetchPokemonData = async (url) => {
   const response = await fetch(url);
   return await response.json();
 };
 
-async function getPokemons() {
+const getPokemons = async function () {
   let arr = [];
   try {
     const data = await fetch(
@@ -91,8 +184,10 @@ async function getPokemons() {
   } catch (e) {
     console.log(e);
   }
-}
+};
 
 cardCatalogButton.addEventListener('click', async () => {
   addAllPokemonCards();
 });
+
+addAllPokemonCards();
